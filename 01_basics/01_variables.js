8bfc1ad7ed723ff76e644eb45d48a9e5924b27ff
Dev Krishan <sa.mael.451.13@gmail.com>
2025-11-14 19:44:16 +0000

    Initial commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+let age = 20 // changeable variable
+const a = 57 // cannot be changed
+var b // var fucks up with the scope so it is recommended to not use it at all
+x = 600
+
+console.log (age)
+console.log (a)
+console.log (b)
+console.log (x)
+
+console.table ([age, a, b, x])
