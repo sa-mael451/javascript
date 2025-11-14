c64e253a7c7e5cb1998dc9dd9b8dcb50bab81c29
Dev Krishan <sa.mael.451.13@gmail.com>
2025-11-14 21:54:38 +0000

    Initial commit

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,17 @@
+let name = "Dev-Krishan"
+let repoCount = 50
+
+console.log (`My name is ${name} and my repo count is ${repoCount}.`);
+
+console.log (name.__proto__);
+console.log (name.length);
+console.log (name.toUpperCase());
+console.log (name.charAt (5));
+console.log (name.substring (1, 4));
+console.log (name.slice (1, 5));
+console.log (name.slice (-5, -1));
+let str = "    dev    "
+console.log (str.trim ());
+console.log (str.replace ('v', 'vil'));
+console.log (name.includes ("Dev")); // returns true or false
+console.log (name.split ('-'));
