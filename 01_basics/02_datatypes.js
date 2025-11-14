4b0cd53e0995222156a1545d69ab79d7933b37a6
Dev Krishan <sa.mael.451.13@gmail.com>
2025-11-14 20:19:33 +0000

    Initial commit

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -6,4 +6,5 @@ let e = null // null
 let big = 12345678901234567890n; //bigint
 // one more 'symbol': these are 7 primitive datatypes in js
 
-console.log(a, b, str, text, c, d, e, big);
+console.log (a, b, str, text, c, d, e, big);
+console.log( typeof (a), typeof str, typeof (c), typeof (d), typeof (e), typeof (big));
---
+++ 01_basics/03_type_conversion.js
@@ -0,0 +1,16 @@
+console.log (Number ('55')); // 55
+console.log (Number ('dev')); // Nan (Not a Number)
+console.log (Number (null)); // 0
+console.log (Number (undefined)); // NaN
+console.log (Number (true)); // 1
+
+console.log (Boolean (0)); // false
+console.log (Boolean (45)); // true
+console.log (Boolean ("")); // false
+console.log (Boolean ("d")); // true
+console.log (Boolean (null)); // false
+console.log (Boolean (undefined)); // false
+
+console.log (String (55));
+console.log (String (null));
+console.log (String (undefined));
