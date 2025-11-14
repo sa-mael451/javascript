181096bcd36f3775e79527f7e01fbccc09c12803
Dev Krishan <sa.mael.451.13@gmail.com>
2025-11-14 19:52:11 +0000

    Initial commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+let a = 56, b = 45.45 // number
+let str = "Dev", text = 'wow' // string
+let c = true // boolean
+let d // undefined
+let e = null // null
+let big = 12345678901234567890n; //bigint
+// one more 'symbol': these are 7 primitive datatypes in js
+
+console.log(a, b, str, text, c, d, e, big);
